0c092d13d27def8979408f8e2639a53a6337992c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-12-20 04:47:08 +0000

    update curd-app

--- CRUDApp/curd_project/curd_project/urls.py
+++
@@ -1,9 +0,0 @@
-# myproject/urls.py
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('curdapp.urls')),
-]
--- CRUDApp/curd_project/curdapp/forms.py
+++
@@ -1,8 +0,0 @@
-# myapp/forms.py
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ('name', 'description',)
