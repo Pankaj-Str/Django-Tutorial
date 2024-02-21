35ad41c81239d3258dc58eb72fe48daede4fc51e
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-21 14:41:02 +0000

    update static

--- Project4/p4n_form/p4n_form/settings.py
+++ Project4/p4n_form/p4n_form/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+
+# STATIC_ROOT = '/static/employee/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR),'employee/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Project4/p4n_form/p4n_form/urls.py
+++ Project4/p4n_form/p4n_form/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('employee/', include('employee.urls')),
+    path('', include('employee.urls')),
 ]
