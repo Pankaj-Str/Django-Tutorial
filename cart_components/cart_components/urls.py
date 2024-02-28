2f9bf5cbebc0613f57a88dec509bbd55f98244a5
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-28 12:22:49 +0530

    update new project

---
+++ cart_components/cart_components/urls.py
@@ -0,0 +1,9 @@
+# myproject/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls')),
+]
