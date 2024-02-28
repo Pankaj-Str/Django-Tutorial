89024b45639a8e29fc20c7ad3449e50bd0f51c9c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-28 15:24:27 +0530

    update setting.py

--- employee_search_project/employee_search_project/settings.py
+++ employee_search_project/employee_search_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee+directory'
+    'employee_directory'
 ]
 
 MIDDLEWARE = [
