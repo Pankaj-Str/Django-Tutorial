ede6407047750ec7244341a472d2cb4f07716ab9
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-11 17:02:42 +0530

    update login

---
+++ mylogin/p4n_login/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from p4n_login import views
+
+app_name = "p4n_login"   
+
+
+urlpatterns = [
+    # path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+]
