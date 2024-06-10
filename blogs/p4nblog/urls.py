5678b0fcec40440039a4250e008835275656b53b
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-06-10 06:47:12 +0000

    update urls.py

---
+++ blogs/p4nblog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import create_post, show_posts
+
+urlpatterns = [
+    path('', create_post, name='create_post'),
+    path('show/', show_posts, name='show_posts'),
+]
--- blogs/p4nblog/views.py
+++ blogs/p4nblog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BlogPost
 
-# Create your views here.
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        BlogPost.objects.create(title=title, content=content)
+        return redirect('show_posts')
+    return render(request, 'create_post.html')
+
+def show_posts(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'show_posts.html', {'posts': posts})
