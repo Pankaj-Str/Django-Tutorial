55ebb4f3546c4ef9aa1969898d19dcaafac1c040
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-08-06 11:08:43 +0530

    Add UserProfile model and update views and templates for user profile display

--- django_Project1/cwpc01/blogs/views.py
+++ django_Project1/cwpc01/blogs/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import UserProfile
+
 def Home(request):
-    return render(request,"home.html")
+    profiles = UserProfile.objects.all()
+    return render(request,"home.html",{'profiles': profiles})
 
 
 def aboutus(request):
